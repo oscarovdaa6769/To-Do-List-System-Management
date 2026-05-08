4142e9e7a903d6929f2e38bfb19224de540e4be4
chhai-cryto <chhai.dev@gmial.com>
2026-05-08 10:14:59 +0700

    login/signup finish 100% by chhai

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,19 @@
 <?php
-
+use App\Http\Controllers\UseraccountController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout.layout');
+    return view('layouts.app');
 });
 
+
+
+Route::get('/login',[LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::get('/signup', [SignupController::class, 'index']);
+Route::post('/signup', [SignupController::class, 'store'])->name('signup.store');
+
+Route::get('/user',[UseraccountController::class, 'index']);
