98bb97727f85df28c7a155e00f63b96d481029df
oscarovdaa6769 <oudomveasnadet67@gmail.com>
2026-05-19 15:20:25 +0700

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Alpine.js -->
-
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
     <div class="flex h-auto font-sans">
